2099a0ba2266e694c47d3dcd523e4c64a5adbc80
Ederson <edersonschaukoski@gmail.com>
2021-05-27 15:02:53 -0300

    Ajustes

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import {
     faTachometerAlt,
     faServer,
 } from "@fortawesome/free-solid-svg-icons";
-
+import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 
 function Home() {
@@ -33,7 +33,9 @@ function Home() {
 
                     <div className="row">
                         <div className="col-lg-4">
-                            <div className={`rounded-circle ${styles.circulo} ${styles.centralizar}`}>
+                            <div
+                                className={`rounded-circle ${styles.circulo} ${styles.centralizar}`}
+                            >
                                 <FontAwesomeIcon icon={faSitemap} />
                             </div>
                             <h2 className="mt-4 mb-4">Personalização</h2>
@@ -43,7 +45,9 @@ function Home() {
                             </p>
                         </div>
                         <div className="col-lg-4">
-                            <div className={`rounded-circle ${styles.circulo} ${styles.centralizar}`}>
+                            <div
+                                className={`rounded-circle ${styles.circulo} ${styles.centralizar}`}
+                            >
                                 <FontAwesomeIcon icon={faTachometerAlt} />
                             </div>
                             <h2 className="mt-4 mb-4">Velocidade</h2>
@@ -52,7 +56,9 @@ function Home() {
                             </p>
                         </div>
                         <div className="col-lg-4">
-                            <div className={`rounded-circle ${styles.circulo} ${styles.centralizar}`}>
+                            <div
+                                className={`rounded-circle ${styles.circulo} ${styles.centralizar}`}
+                            >
                                 <FontAwesomeIcon icon={faServer} />
                             </div>
                             <h2 className="mt-4 mb-4">Manutenção</h2>
@@ -64,24 +70,28 @@ function Home() {
                     </div>
                     <hr className="featurette-divider" />
                     <div id="portfolio" className="row featurette">
-                        <div className="col-md-7" style={{margin: "auto"}}>
-                            <h2 className="featurette-heading">
-                                Portfólio
-                            </h2>
-                            <p className="lead">
-                                Foi criado o site da evipes!
-                            </p>
+                        <div className="col-md-7" style={{ margin: "auto" }}>
+                            <h2 className="featurette-heading">Portfólio</h2>
+                            <p className="lead">Foi criado o site da evipes!</p>
                         </div>
                         <div className="col-md-5">
-                            <img
-                                className="featurette-image img-fluid mx-auto"
-                                data-src="holder.js/500x500/auto"
-                                alt="500x500"
-                                style={{width: "500px", height: "500px"}}
-                                src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22500%22%20height%3D%22500%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20500%20500%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_179aa849c76%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_179aa849c76%22%3E%3Crect%20width%3D%22500%22%20height%3D%22500%22%20fill%3D%22%23EEEEEE%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22185.1171875%22%20y%3D%22261.1%22%3E500x500%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
-                                data-holder-rendered="true"
-                            />
+                            <a
+                                href="https://edersonschaukoski.herokuapp.com/evipes/"
+                                target="blank"
+                            >
+                                <Image
+                                    src="/evipes.png"
+                                    alt="Evipes"
+                                    width={500}
+                                    height={500}
+                                />
+                            </a>
                         </div>
+                    </div>
+                    <hr className="featurette-divider" />
+                    <div id="contato">
+                        <h2 className="mt-4 mb-4">Contato</h2>
+                        <p>Email: eder-duarte@hotmail.com.br</p>
                     </div>
                 </Container>
             </div>
